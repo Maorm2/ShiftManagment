d1e3e4559bea31832747726d50be4151d8a914fd
MaorMin <maorminyan@gmail.com>
2020-05-13 12:10:48 +0300

    Connection to firebase

--- app/src/main/java/com/example/shiftmanagment/database/Database.java
+++ app/src/main/java/com/example/shiftmanagment/database/Database.java
@@ -1,6 +1,14 @@
 package com.example.shiftmanagment.database;
 
+import androidx.annotation.NonNull;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Database {
+
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     private static Database instance = null;
     private Database(){}
@@ -11,5 +19,42 @@ public class Database {
         return instance;
     }
 
+
+    //sign up users
+    public void createUser(final String username,final String password){
+        mAuth.createUserWithEmailAndPassword(username,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+
+                }
+                else
+                {
+
+                }
+            }
+        });
+    }
+
+    //sign in the user
+    public void signInUser(final String username,final String password){
+        mAuth.signInWithEmailAndPassword(username,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+
+                }
+                else
+                {
+
+                }
+            }
+        });
+    }
+
+    //sign out the user
+    public void signOutUser(){
+        mAuth.signOut();
+    }
 
 }
