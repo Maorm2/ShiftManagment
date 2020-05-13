af27f595c5811b203670659d287fd70461ba2a20
MaorMin <maorminyan@gmail.com>
2020-05-13 10:47:21 +0300

    first commit

--- app/src/main/java/com/example/shiftmanagment/view/MainActivity.java
+++ app/src/main/java/com/example/shiftmanagment/view/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.shiftmanagment;
+package com.example.shiftmanagment.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.shiftmanagment.R;
 
 public class MainActivity extends AppCompatActivity {
 
