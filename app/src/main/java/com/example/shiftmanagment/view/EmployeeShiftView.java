4375b07bb467e39b46735713f81a61b9ae936342
Haim Petel <haimp2@gmail.com>
2020-06-02 07:31:42 +0300

    calendar view added

--- app/src/main/java/com/example/shiftmanagment/view/EmployeeShiftView.java
+++ app/src/main/java/com/example/shiftmanagment/view/EmployeeShiftView.java
@@ -2,10 +2,22 @@ package com.example.shiftmanagment.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.shiftmanagment.R;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
+import org.threeten.bp.DayOfWeek;
+import org.threeten.bp.LocalDate;
+import org.threeten.bp.LocalDateTime;
+import org.threeten.bp.temporal.TemporalAdjuster;
+import org.threeten.bp.temporal.TemporalAdjusters;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class EmployeeShiftView extends AppCompatActivity {
 
@@ -13,9 +25,12 @@ public class EmployeeShiftView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee_shift_view);
+
+        LocalDate nextSunday = LocalDate.now().with(TemporalAdjusters.next(DayOfWeek.SUNDAY));
+
+        MaterialCalendarView materialCalendarView = findViewById(R.id.calendarView);
+        materialCalendarView.state().edit().setMinimumDate(nextSunday)
+                .setMaximumDate(CalendarDay.from(nextSunday.plusDays(7))).commit();
     }
 
-    public void onBtnClick(View v){
-        v.getId();
-    }
 }
